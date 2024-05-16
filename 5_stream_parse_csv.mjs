78b7c917b1101d1154ae67b5d3b4b82929393412
joechea <s.chea@aupp.edu.kh>
2024-05-16 16:05:47 +0700

    add csv parse

---
+++ 5_stream_parse_csv.mjs
@@ -0,0 +1,8 @@
+import * as csv from "csv-parse";
+import fs from "fs";
+
+fs.createReadStream("log.csv")
+  .pipe(csv.parse({ delimiter: "," }))
+  .on("data", function (row) {
+    console.log(row);
+  });
